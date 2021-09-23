d03a00e0a6f2f3c39eccb03c39e57093b8c0b416
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-09-23 21:45:06 +0900

    jpaConfig @EnableJpaAuditing 세팅 무의미

---
+++ src/main/java/com/example/dm0815/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.example.dm0815.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+//@Configuration
+//@EnableJpaAuditing
+// Dm0815Application에서 이미 @EnableJpaAuditing 했어!
+public class JpaConfig {
+}
